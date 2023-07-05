9cb1df49778fd6a69f5cc880df1ec9513b5125c5
helenasokabe <helena.sokabe@hotmail.com>
2023-07-05 18:40:00 -0300

    Implementação do salvar

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,36 +7,42 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
+import java.sql.SQLException;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    public int cadastrarProduto(ProdutosDTO produto) {
+        conn = new conectaDAO().connectDB();
+
+        int status;
+        try {
+            prep = conn.prepareStatement("insert into produtos (nome, valor, status) VALUES(?,?,?)");
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            status = prep.executeUpdate();
+
+            return status; //retornar 1
+        } catch (SQLException ex) {
+            System.out.println("Erro ao conectar: " + ex.getMessage());
+            return ex.getErrorCode();
+        }
+
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
